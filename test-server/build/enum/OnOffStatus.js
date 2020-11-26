20a06f5a746f7b49ed87b575e7aa8fa4a5b9fa4f
naciaydinalp <naciaydinalp@gmail.com>
2020-11-26 16:00:24 +0300

    del

--- test-server/build/enum/OnOffStatus.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var OnOffStatus;
-(function (OnOffStatus) {
-    OnOffStatus["On"] = "On";
-    OnOffStatus["Off"] = "Off";
-})(OnOffStatus = exports.OnOffStatus || (exports.OnOffStatus = {}));
-;
--- test-server/build/rest-api/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const area_1 = require("./area");
-const factory_1 = require("./factory");
-exports.default = (app, db) => {
-    app.use('/api/area', area_1.default(db));
-    app.use('/api/factory', factory_1.default(db));
-    console.log(db.models);
-};
